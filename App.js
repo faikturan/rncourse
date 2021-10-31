5ec59f63d22c21e7d51039e214d072fb2b33715d
FAIK TURAN <turanfaik@hotmail.com>
2021-10-31 16:39:15 +0300

    Flex konumlandırma

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.slideOne}></View>
+        <View style={styles.slideTwo}></View>
     </View>
   );
 }
@@ -14,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFEB3B',
   },
+    slideOne: {
+      backgroundColor: '#607D8B',
+       flex:1
+    },
+    slideTwo: {
+        backgroundColor: '#FF5722',
+        flex:2
+    },
 });
