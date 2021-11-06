f4347c8ba9e882be8c9567de7f30708ef042e7e8
TURKCELL\faik.turan <faik.turan@consultant.turkcell.com.tr>
2021-11-06 18:47:30 +0300

    Image 2 resizdeMode anlatıldı

--- App.js
+++ App.js
@@ -2,11 +2,20 @@ import React, {Component} from 'react';
 import { Platform, StyleSheet, Image, View, } from 'react-native';
 
 export default class App extends Component {
+    /*
+    resizeMode
+    -cover
+    -contain
+    -stretch
+    -repeat
+    -center
+     */
     render(){
         return (
             <View style={styles.container}>
-            <Image style={{width:'100%', height:180}}
-                       source={require('./src/assets/sweden.jpeg')} />
+            <Image resizeMode={'stretch'}
+                style={{width:'100%', height:180, borderWidth:2, borderColor:'red'}}
+                       source={{uri:'http://via.placeholder.com/500X200'}} />
             </View>
     );
     }
